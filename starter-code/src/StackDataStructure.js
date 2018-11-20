52c121f684b9dc834d6b6b30e64a53b4e5f79c7a
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2018-11-19 23:03:02 -0600

    Work in progress

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,39 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 10;
+
+  this.isEmpty = function () {
+    if( this.stackControl.length === 0){
+      return true
+    } else{
+      return false
+    }
+  }
+
+  this.canPush = function(){
+    if (this.stackControl.length <= this.MAX_SIZE) {
+      return true
+    } else{
+      return "Stack Overflow"
+    }
+  }
+  this.push = function(element){
+    if(this.canPush){
+    this.stackControl.push(element)
+    return this.stackControl
+    } else {
+      return false
+    }
+  };
+
+  this.pop = function(element){
+    if (this.isEmpty) {
+      return this.stackControl.pop(element)
+    }else{
+      return "Stack Underflow"
+    }
+  }
+ 
 }
+
+
